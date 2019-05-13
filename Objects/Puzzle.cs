1d1fb9a50e58161b4cf9ae963bbefaffaff15b3b
NerdAlert58 <bushyhead@gmail.com>
2019-05-13 16:20:41 -0500

    updates so far

--- Objects/Puzzle.cs
+++ Objects/Puzzle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SudokuSolver.Objects
@@ -65,7 +66,102 @@ namespace SudokuSolver.Objects
                 _table[i / 9, i % 9] = Convert.ToInt32(strArray[i]);
             }
 
+            Validate();
+
             return true;
+        }
+
+        public void Solve()
+        {
+            while (!_finished)
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    for (int j = 0; j < 9; j++)
+                    {
+                        var remaining = SimpleFills(i, j);
+                        System.Console.WriteLine(remaining.Count);
+                        switch (remaining.Count)
+                        {
+                            case 1:
+                                _table[i, j] = remaining.ToList()[0];
+                                Display();
+                                break;
+                            case 2:
+                            case 3:
+                            case 4:
+                            case 5:
+                            case 6:
+                            case 7:
+                            case 8:
+                            case 9:
+
+                                break;
+                            default:
+                                // Do nothing
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private HashSet<int> SimpleFills(int i, int j)
+        {
+            var known = new HashSet<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            if (_table[i, j] == 0)
+            {
+                return known;
+            }
+            // check row
+            var row = i;
+            var column = j;
+            for (int k = 0; k < 9; k++)
+            {
+                if (_table[row, k] == 0) continue;
+                known.Remove(_table[row, k]);
+            }
+            // check column
+            for (int l = 0; l < 9; l++)
+            {
+                if (_table[l, column] == 0) continue;
+                known.Remove(_table[l, column]);
+            }
+            // check square
+            if (i < 3)
+            {
+                row = 0;
+            }
+            else if (i >= 3 && i < 6)
+            {
+                row = 3;
+            }
+            else
+            {
+                row = 6;
+            }
+            if (j < 3)
+            {
+                column = 0;
+            }
+            else if (j >= 3 && j < 6)
+            {
+                column = 3;
+            }
+            else
+            {
+                column = 6;
+            }
+
+            for (int m = row; m < (row + 3); m++)
+            {
+                for (int n = column; n < (column + 3); n++)
+                {
+                    if (_table[m, n] == 0) continue;
+                    known.Remove(_table[m, n]);
+                }
+            }
+            return known;
         }
 
         private bool Validate()
@@ -74,26 +170,45 @@ namespace SudokuSolver.Objects
             // Check rows
             for (int i = 0; i < 9; i++)
             {
-                var found = new List<int>();
+                var rows = new List<int>();
+                var columns = new List<int>();
                 for (int j = 0; j < 9; j++)
                 {
-                    if (found.Contains(_table[i, j])) return false;
-                    found.Add(_table[i, j]);
-                }
-            }
-
-            // Check columns
-            for (int i = 0; i < 9; i++)
-            {
-                var found = new List<int>();
-                for (int j = 0; j < 9; j++)
-                {
-                    if (found.Contains(_table[j, i])) return false;
-                    found.Add(_table[j, i]);
+                    if (_table[i, j] != 0 && rows.Contains(_table[i, j])) return false;
+                    if (_table[j, i] != 0 && columns.Contains(_table[j, i])) return false;
+                    rows.Add(_table[i, j]);
+                    columns.Add(_table[j, i]);
                 }
             }
 
             // Check squares
+            var leftSquare = new List<int>();
+            var middleSquare = new List<int>();
+            var rightSquare = new List<int>();
+            for (int i = 0; i < 9; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    leftSquare.Clear();
+                    middleSquare.Clear();
+                    rightSquare.Clear();
+                }
+                for (int j = 0; j < 3; j++)
+                {
+                    if (_table[i, j] != 0 && leftSquare.Contains(_table[i, j])) return false;
+                    leftSquare.Add(_table[i, j]);
+                }
+                for (int j = 3; j < 6; j++)
+                {
+                    if (_table[i, j] != 0 && middleSquare.Contains(_table[i, j])) return false;
+                    middleSquare.Add(_table[i, j]);
+                }
+                for (int j = 6; j < 9; j++)
+                {
+                    if (_table[i, j] != 0 && rightSquare.Contains(_table[i, j])) return false;
+                    rightSquare.Add(_table[i, j]);
+                }
+            }
 
             return true;
         }
