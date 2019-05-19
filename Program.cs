0fab12b57a315f19cf89e18e453680a37a9d613b
Scott Bushyhead <Bushyhead@gmail.com>
2019-05-19 16:21:54 -0500

    Create solve method  (#1)
    * updates so far
    
    * Base Solve case created.
    
    * fixed conditional

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace SudokuSolver
         {
             var puzzle = new Puzzle();
             puzzle.LoadTable(",,,5,,,4,,7,,4,,,6,,,,,,6,8,,,3,,,5,,,3,,,4,,,,5,,2,,,,8,,4,,,,1,,,7,,,9,,,3,,,1,7,,,,,,2,,,9,,2,,1,,,9,,,");
-            puzzle.Display();
+            puzzle.Solve();
         }
     }
 }
